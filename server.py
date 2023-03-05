1240fa3931115ef9a1c986f55fcb3b6b3d94d3da
CarlosMena01 <cmena@unal.edu.co>
2023-03-05 13:23:24 -0500

    Render from template

--- server.py
+++ server.py
@@ -1,18 +1,15 @@
 # Librerias para stream 
 from fastapi import FastAPI, Response
-from time import sleep
+from starlette.responses import StreamingResponse
 # Librerías de manejo de imagenes
-from picamera import PiCamera
-from io import BytesIO
 import numpy as np
 from scipy.fftpack import fft2, fftshift
 import cv2
 
 app = FastAPI()
 
-# Configuramos la resolución de la cámara
-camera = PiCamera()
-camera.resolution = (640, 480)
+# Configuramos la cámara
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 # Función para aplicar la transformada de Fourier a una imagen
 def apply_fourier_transform(frame):
@@ -25,34 +22,26 @@ def apply_fourier_transform(frame):
     magnitude_spectrum = 20*np.log(np.abs(fshift))
     
     
-    return magnitude_spectrum
+    return frame
 
-# Ruta para obtener el stream de video con la transformada de Fourier
+def generate():
+     while True:
+          ret, frame = cap.read()
+          if ret:
+                frame = apply_fourier_transform(frame)
+                (flag, encodedImage) = cv2.imencode(".jpg", frame)
+                if not flag:
+                   continue
+                yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+@app.get("/")
+def index():
+    with open("./templates/index.html") as fh:
+        data = fh.read()
+    return Response(content=data, media_type="text/html")
+
 @app.get("/video_feed")
-async def video_feed(response: Response):
-    response.headers["Content-Type"] = "multipart/x-mixed-replace; boundary=frame"
+def video_feed():
+    return StreamingResponse(generate(), media_type="multipart/x-mixed-replace;boundary=frame")
 
-    # Configuramos la cámara para obtener un stream continuo de video
-    camera.framerate = 24
-    camera.start_preview()
-    sleep(2) # Esperamos a que la cámara se estabilice
 
-    # Generamos el stream de video
-    while True:
-        # Capturamos una imagen de la cámara
-        stream = BytesIO()
-        camera.capture(stream, format='jpeg', use_video_port=True)
-        frame = cv2.imdecode(np.frombuffer(stream.getvalue(), dtype=np.uint8), -1)
-
-        # Aplicamos la transformada de Fourier a la imagen
-        frame = apply_fourier_transform(frame)
-
-        # Enviamos la imagen como parte del stream de video
-        response_body = b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + cv2.imencode('.jpg', frame)[1].tostring() + b'\r\n'
-        try:
-            await response.write(response_body)
-        except:
-            break
-
-    # Paramos la cámara al terminar el stream de video
-    camera.stop_preview()
+cap.release()
