3c535101066b0e186c69e6814854a916e8512161
CarlosMena01 <cmena@unal.edu.co>
2023-03-05 20:19:41 -0500

    [FIX] App Host

--- server.py
+++ server.py
@@ -53,6 +53,6 @@ def video_feed():
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False, host='0.0.0.0')
 
 cap.release()
